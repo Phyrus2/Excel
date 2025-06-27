5f35f96257ebab489b96be4a7fa1293908b00f87
Yuda <madeyudaadiwinata@gmail.com>
2025-06-27 14:55:58 +0800

    Deploy Angular app to GitHub Padaswers

--- docs/server/chunk-HOITPF6C.mjs
+++ docs/server/chunk-HOITPF6C.mjs
@@ -129,6 +129,8 @@ Please note that your pick-up time will be between ${r.PickupTime} - ${D} AM at 
 
 For tomorrow we are scheduled to depart at 7.00 AM from Sanur port. When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards.
 
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
+
 To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, sneakers, apply sunscreen, and bring sunglasses.  Feel free to bring your swimsuit. You will have the chance to go for a swim, especially when visiting Diamond Beach. 
 
 Nusa Penida is a relatively new destination that is not fully developed yet, giving you a glimpse of Bali as it was 30 years ago. Approximately 20% of the roads in Nusa Penida are still bumpy, and public facilities are limited. Due to the narrow roads, we may encounter some traffic jams while moving from one spot to another.
@@ -218,6 +220,8 @@ Please note that your pick-up time will be between ${r.PickupTime} - ${D} AM at 
 Sorry for this inconvenience that we have to pick you up slightly earlier than usual, cause of pickup distance from the harbor and for tomorrow we are scheduled depart at 7.00 AM from Sanur port, so that we can arrive in Nusa Penida earlier and cover all the destinations as planned specially for taking photo at tree house. Please be informed, this is a group tour and on rare occasions, some participants may not be punctual. However, rest assured that we will inform you in case of any delays when picking you up. Please don't worry, as you will still be picked up as scheduled. 
 
 When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards. 
+
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
 
 If you would like to spend extra time swimming at a beach, you will have that time when you visit Diamond Beach. To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, apply sunscreen, and bring sunglasses. 
 
@@ -637,6 +641,8 @@ Please note that your pick-up time will be between ${r.PickupTime} - ${D} AM fro
 
 For tomorrow we are scheduled depart at 7.00 AM from Sanur port. When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards Privately.
 
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
+
 To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, sneakers, apply sunscreen, and bring sunglasses.  Feel free to bring your swimsuit. You will have the chance to go for a swim, especially when visiting Diamond Beach. 
 
 Nusa Penida is a relatively new destination that is not fully developed yet, giving you a glimpse of Bali as it was 30 years ago. Approximately 20% of the roads in Nusa Penida are still bumpy, and public facilities are limited. Due to the narrow roads, we may encounter some traffic jams while moving from one spot to another.
@@ -856,6 +862,8 @@ Please note that your pick-up time will be between ${r.PickupTime} - ${O} AM at 
 
 For tomorrow we are scheduled to depart at 7.00 AM from Sanur port. When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards.
 
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
+
 To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, sneakers, apply sunscreen, and bring sunglasses.  Feel free to bring your swimsuit. You will have the chance to go for a swim, especially when visiting Diamond Beach. 
 
 Nusa Penida is a relatively new destination that is not fully developed yet, giving you a glimpse of Bali as it was 30 years ago. Approximately 20% of the roads in Nusa Penida are still bumpy, and public facilities are limited. Due to the narrow roads, we may encounter some traffic jams while moving from one spot to another.
@@ -945,6 +953,8 @@ Please note that your pick-up time will be between ${r.PickupTime} - ${O} AM at 
 Sorry for this inconvenience that we have to pick you up slightly earlier than usual, cause of pickup distance from the harbor and for tomorrow we are scheduled depart at 7.00 AM from Sanur port, so that we can arrive in Nusa Penida earlier and cover all the destinations as planned specially for taking photo at tree house. Please be informed, this is a group tour and on rare occasions, some participants may not be punctual. However, rest assured that we will inform you in case of any delays when picking you up. Please don't worry, as you will still be picked up as scheduled. 
 
 When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards. 
+
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
 
 If you would like to spend extra time swimming at a beach, you will have that time when you visit Diamond Beach. To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, apply sunscreen, and bring sunglasses. 
 
@@ -1386,6 +1396,8 @@ EXCLUDED:
 Please note that your pick-up time will be between ${r.PickupTime} - ${O} AM at ${r.Location}. The driver will assist you with the check in process in Bali harbor. Please be informed that this is a group tour, and on rare occasions, some participants may not be punctual. However, rest assured that we will inform you in case of any delays when picking you up. Please don't worry, as you will still be picked up as scheduled
 
 For tomorrow we are scheduled to depart at 7.00 AM from Sanur port. When you arrive in Nusa Penida, please be attentive and look for our team holding a white paper sign with your name on it. Your tour will be arranged by our team from this point onwards.
+
+Kindly note that this tour involves moderate walking and the use of stairs, particularly at Diamond Beach and Kelingking Beach. While it is not mandatory to trek all the way down, guests are welcome to explore further, and our guide will be happy to accompany you as long as you feel comfortable.
 
 To ensure your comfort throughout the trip, it is recommended that you wear comfortable clothing, walking shoes, sneakers, apply sunscreen, and bring sunglasses.  Feel free to bring your swimsuit. You will have the chance to go for a swim, especially when visiting Diamond Beach. 
 
